702456ae00ccf081bf2b0f0f292dcd7eac8ed254
Ali <ali.sarwat12345@gmail.com>
2025-04-28 22:43:50 +0400

    updated TransMatrix

--- SpaceFrame/TransMatrix.py
+++ SpaceFrame/TransMatrix.py
@@ -1,58 +1,29 @@
-import torch    
+from functools import cached_property
 
+from LineElements import LineElements
+
+import numpy as np    
 class TransMatrix:
     
-    def __init__(self):
-        pass
+    def __init__(self, elements: LineElements):
+        self._elements = elements
     
-    def transMatrix3x3(self, numElements : int, elementsProperties : torch.Tensor, deltaNodes : torch.Tensor):
-        r = torch.zeros((numElements, 3, 3), dtype=torch.float64)
-        
-        L   = elementsProperties[:, 0]
-        psi = elementsProperties[:, 11]
-        
-        rxX = deltaNodes[:, 0]/L
-        rxY = deltaNodes[:, 1]/L
-        rxZ = deltaNodes[:, 2]/L
-        
-        r[:, 0, 0] = rxX
-        r[:, 0, 1] = rxY
-        r[:, 0, 2] = rxZ
-        
-        # elements local x-axis is aligned with the global Y-axis
-        isAligned  = (rxX == 0) & (rxZ == 0) 
-        
-        rxXY   = rxX[~isAligned]*rxY[~isAligned]
-        rxYZ   = rxY[~isAligned]*rxZ[~isAligned]
-        rxXZ_2 = torch.sqrt(rxX[~isAligned]**2 + rxZ[~isAligned]**2)
-        
-        psi        = torch.deg2rad(psi)
-        cosPsi     = torch.cos(psi)
-        sinPsi     = torch.sin(psi)
-
-        r[~isAligned, 1, 0] = (-rxXY*cosPsi[~isAligned] - rxZ[~isAligned]*sinPsi[~isAligned])/rxXZ_2
-        r[~isAligned, 1, 1] = rxXZ_2*cosPsi[~isAligned]
-        r[~isAligned, 1, 2] = (-rxYZ*cosPsi[~isAligned] + rxX[~isAligned]*sinPsi[~isAligned])/rxXZ_2
-        
-        r[~isAligned, 2, 0] = (rxXY*sinPsi[~isAligned] - rxZ[~isAligned]*cosPsi[~isAligned])/rxXZ_2
-        r[~isAligned, 2, 1] = -rxXZ_2*sinPsi[~isAligned]
-        r[~isAligned, 2, 2] = (rxYZ*sinPsi[~isAligned] + rxX[~isAligned]*cosPsi[~isAligned])/rxXZ_2
-        
-        r[isAligned, 1, 0] = -rxY[isAligned]*cosPsi[isAligned]
-        r[isAligned, 1, 2] = sinPsi[isAligned]
-
-        r[isAligned, 2, 0] = rxY[isAligned]*sinPsi[isAligned]
-        r[isAligned, 2, 2] = cosPsi[isAligned]
-        
-        return r
-
-    def transMatrix12x12(self, numElements : int, transMatrix3x3 : torch.Tensor):
-        T = torch.zeros((numElements, 12, 12), dtype=torch.float64)
+    @cached_property
+    def trans_matrix_3x3(self):
+        return self._elements.direction_cosines
     
-        T[:, 0 : 3, 0 : 3]   = transMatrix3x3
-        T[:, 3 : 6, 3 : 6]   = transMatrix3x3
-        T[:, 6 : 9, 6 : 9]   = transMatrix3x3
-        T[:, 9 : 12, 9 : 12] = transMatrix3x3
+    @cached_property
+    def trans_matrix_12x12(self):   
+        t3x3 = self.trans_matrix_3x3
+        t12x12 = np.zeros((self._elements.num_elements, 12, 12), dtype=np.float64)
         
-        return T
+        t12x12[:, 0:3, 0:3]   = t3x3
+        t12x12[:, 3:6, 3:6]   = t3x3
+        t12x12[:, 6:9, 6:9]   = t3x3
+        t12x12[:, 9:12, 9:12] = t3x3
     
+        return t12x12
+    
+    @cached_property
+    def trans_matrix_12x12_T(self):
+        return self.trans_matrix_12x12.transpose(0, 2, 1)
