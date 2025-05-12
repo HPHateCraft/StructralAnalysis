af8a4d57b4d29db4fee41efbe2fecc6d3b440169
Ali <ali.sarwat12345@gmail.com>
2025-05-12 16:16:48 +0400

    commit 5

--- SpaceFrame/__init__.py
+++ SpaceFrame/__init__.py
@@ -1,8 +1,8 @@
-from .Nodes import Nodes
-from .Materials import Materials
-from .Sections import Sections
-from .LineElements import LineElements
-from .TransMatrix import TransMatrix
-from .StiffnessMatrix import StiffnessMatrix
-from .Loads import Loads
-from .Solver import Solver
+# from .Nodes import Nodes
+# from .Materials import Materials
+# from .Sections import Sections
+# from .LineElements import LineElements
+# from .TransMatrix import TransMatrix
+# from .StiffnessMatrix import StiffnessMatrix
+# from .Loads import Loads
+# from .Solver import Solver
