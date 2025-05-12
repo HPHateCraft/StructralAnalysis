80a5580f966de3993b116d3853e06d56657c9c2d
Ali <ali.sarwat12345@gmail.com>
2025-05-12 20:03:12 +0400

    15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 
@@ -22,7 +22,6 @@ release = '0.0.1'
 
 extensions = [
     'sphinx.ext.autodoc',      
-    'sphinx.ext.napoleon',     
     'sphinx.ext.viewcode',     
     'sphinx.ext.githubpages',  
     'sphinx_rtd_theme',
